9a271a42eea95faf77c20ebd13a870c0b60abbee
tyanyaw <tyacode@gmail.com>
2024-05-04 18:29:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="todo-app">
+    <h1>ToDosss</h1>
+    <section>
+      <ul>
+        <li><ToDoItem label="Hello" v-bind:done="false" /></li>
+        <li><ToDoItem label="Lagi" v-bind:done="true" /></li>
+        <li><ToDoItem label="Bambang" v-bind:done="false" /></li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue'
+export default {
+  name: 'ToDoApp',
+  components: {
+    ToDoItem
+  }
+}
+</script>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <article>
+    <input type="checkbox" id="todo-item" v-bind:checked="isDone" />
+    <label for="todo-item">{{ label }}</label>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ToDoItem',
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean }
+  },
+  data() {
+    return {
+      isDone: this.done
+    }
+  }
+}
+</script>
